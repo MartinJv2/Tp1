a792ca549f5b3d2c38bcbb578885e0c9525c80db
martinj <martinj@educ.college-montreal.qc.ca>
2022-09-15 09:18:42 -0400

    Commit 2

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-text = (str(input("Écrivez une phrase")))
+def word_count():
+    text = (str(input("Écrivez une phrase")))
 
-print("Le nombre de mots dans votre phrase est de : " + str(len(text.split(" "))))
+    print("Le nombre de mots dans votre phrase est de : " + str(len(text.split(" "))))
+
+word_count()
